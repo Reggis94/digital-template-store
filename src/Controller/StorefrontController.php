3d69f4e8819a6b3751a287d3fb37c2396d2f0342
Reggis94 <regisboamah@gmail.com>
2025-04-22 21:18:28 +0200

    Hard coded download file

--- src/Controller/StorefrontController.php
+++ src/Controller/StorefrontController.php
@@ -7,6 +7,12 @@ use App\Repository\ProductRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use App\Entity\Marketing;
+
 
 class StorefrontController extends AbstractController
 {
@@ -18,6 +24,53 @@ class StorefrontController extends AbstractController
         $products = $productRepository->findAll();
         return $this->render('storefront/index.html.twig', [
             'products' => $products,
+        ]);
+    }
+
+    //Use sessions if email is already submitted
+    #[Route('/download-email-form/{id}', name: 'storefront_download_email_form', methods: ['GET', 'POST'])]
+    public function downloadEmailForm(int $id, Request $request, EntityManagerInterface $em){
+        $product = $em->getRepository(Product::class)->find($id);
+        if (!$product) {
+            throw $this->createNotFoundException('Product not found');
+        }
+
+        $form = $this->createFormBuilder()->add('email', EmailType::class)->add('emailDownloadSubmit', SubmitType::class)->getForm();
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $marketing = new Marketing();
+            $data = $form->getData();
+            $email = $data['email'];
+            $marketing->setEmail($email);
+            $marketing->setCreatedAt(new \DateTimeImmutable('now', new \DateTimeZone('UTC')));
+            $em->persist($marketing);
+            $em->flush();
+
+            return $this->render('storefront/download_button.html.twig', [
+                'id' => $id,
+                'product' => $product,
+            ]);
+        }
+        
+        return $this->render('storefront/download_email_form.html.twig', [
+            'form' => $form->createView(),
+            'id' => $id,
+            'product' => $product,
+        ]);
+    }
+
+    #[Route('/download/{id}', name: 'storefront_download', methods: ['GET'])]
+    public function download(int $id, Request $request, EntityManagerInterface $em){
+        $product = $em->getRepository(Product::class)->find($id);
+        if (!$product) {
+            throw $this->createNotFoundException('Product not found');
+        }
+        $finder = 
+        //Public path to the file
+        $file = $this->getParameter('kernel.project_dir') . '\public\uploads\products\\' . $product->getFolderPath() . '\download\etsy-instructions-0001.pdf';
+        return new BinaryFileResponse($file, 200, [
+            'Content-Type' => 'application/pdf',
+            'Content-Disposition' => 'attachment; filename="thank_you_and_instructions_template.pdf"',
         ]);
     }
 }
---
+++ src/Repository/MarketingRepository.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Marketing;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Marketing>
+ */
+class MarketingRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Marketing::class);
+    }
+
+    //    /**
+    //     * @return Marketing[] Returns an array of Marketing objects
+    //     */
+    //    public function findByExampleField($value): array
+    //    {
+    //        return $this->createQueryBuilder('m')
+    //            ->andWhere('m.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->orderBy('m.id', 'ASC')
+    //            ->setMaxResults(10)
+    //            ->getQuery()
+    //            ->getResult()
+    //        ;
+    //    }
+
+    //    public function findOneBySomeField($value): ?Marketing
+    //    {
+    //        return $this->createQueryBuilder('m')
+    //            ->andWhere('m.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->getQuery()
+    //            ->getOneOrNullResult()
+    //        ;
+    //    }
+}
